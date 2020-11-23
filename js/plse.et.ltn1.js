cfba85439ff1da53f0921dac434c5d47851f0e9d
JinjoGuy <59514927+JinjoGuy@users.noreply.github.com>
2020-11-23 18:17:02 -0300

    Create plse.et.ltn1.js

---
+++ js/plse.et.ltn1.js
@@ -0,0 +1,72 @@
+* Javascript library containing equivalence table for Layton 1 */
+
+aade.equivalenceTable = {
+	256: {
+		'original': '---',
+		'adapted': '---'
+	},
+	512: {
+		'original': '---',
+		'adapted': '---'
+	},
+	2816: {
+		'original': 'Layton',
+		'adapted': 'Leiton'
+	},
+	768: {
+		'original': 'Luke',
+		'adapted': 'Lucas'
+	},
+	4352: {
+		'original': 'Dahlia',
+		'adapted': 'Daisy'
+	},
+	1792: {
+		'original': 'Gordon',
+		'adapted': 'Gordon'
+	},
+	2304: {
+		'original': 'Matthew',
+		'adapted': 'Matthias'
+	},
+	2560: {
+		'original': 'Ramon',
+		'adapted': 'Madruga'
+	},
+	1024: {
+		'original': 'Simon',
+		'adapted': 'Vamorea'
+	},
+	4096: {
+		'original': 'Granny R.',
+		'adapted': 'Granny E.'
+	},
+	5888: {
+		'original': 'Agnes',
+		'adapted': 'Joan'
+	},
+	5376: {
+		'original': 'Adrea',
+		'adapted': 'Bonnie'
+	},
+	6144: {
+		'original': 'Archibald',
+		'adapted': 'Facenton'
+	},
+	4864: {
+		'original': 'Amelie',
+		'adapted': 'Amelia'
+	},
+	5120: {
+		'original': 'Beatrice',
+		'adapted': 'Bea'
+	},
+	1536: {
+		'original': 'Crouton',
+		'adapted': 'Cretoln'
+	},
+	3840: {
+		'original': 'Grumm',
+		'adapted': 'Michaeleas'
+	}
+}; 
